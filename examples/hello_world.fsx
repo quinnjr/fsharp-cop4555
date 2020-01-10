edf0bfaee09fa21c42556c0d418534c5ef8c85d8
Joseph R. Quinn <423821+quinnjr@users.noreply.github.com>
2020-01-09 21:00:28 -0500

    Adding 'hello_world' example.'

---
+++ examples/hello_world.fsx
@@ -0,0 +1,13 @@
+// Hello World example
+//
+// Simply prints "Hello World" using F#'s built-in `printfn` function.
+// Adds a `main` function to the function call and a `printit` function
+// with a string parameter to showcase Functional Programming style.
+
+let printit str =
+  printfn "%s" str
+
+let main =
+  printit "Hello World"
+
+main
