871eb5fbe334622adc84aee8171cff5c5b0e5867
Joseph R. Quinn <423821+quinnjr@users.noreply.github.com>
2020-01-10 11:46:47 -0500

    Adding a second Hello World with unit type parameter.

---
+++ examples/hello_world_2.fsx
@@ -0,0 +1,10 @@
+// Hello World example
+//
+// Simply prints "Hello World" using F#'s built-in `printfn` function.
+// Adds a `main` function to the function call and `printfn` taking a
+// unit type as a parameter.
+
+let main () =
+  printfn "%s" "Hello World"
+
+main()
