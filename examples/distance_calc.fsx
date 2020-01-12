5ae891494b64bf1dab34342bac7c89675d902a48
Joseph R. Quinn <423821+quinnjr@users.noreply.github.com>
2020-01-12 12:39:24 -0500

    Adding additional comments to distance_calc. Changing assignment in distance_calc to be more ergonomic.

--- examples/distance_calc.fsx
+++ examples/distance_calc.fsx
@@ -1,3 +1,10 @@
+// A simple calculator for the distance between two points on a
+// 2D plane.
+// Input from the commandline comes in the form of
+// `x1` `y1` `x2` `y2`, where x's are the x-coordinates of the two
+// two points and y's are the y coordinates of the two points.
+
+// Calculate the distance between Point 1 and Point 2.
 // Note: Powers in F# on floats use the pown function.
 // - ** is for floating point values, not integers.
 // - ^ is not usable.
@@ -8,17 +15,19 @@ let distance (x1: float) (y1: float) (x2: float) (y2: float) : float =
   sqrt (x_f + y_f)
 
 let main () =
+  // Convert commandline string arguements into an Array and remove
+  // the first element (the command executing).
   let args: string array = fsi.CommandLineArgs |> Array.tail
-  // TODO: Single `let` statement
-  let x1 = (args.[0] |> float)
-  let y1 = (args.[1] |> float)
-  let x2 = (args.[2] |> float)
-  let y2 = (args.[3] |> float)
 
-  printfn "Finding the distance between Point (%f, %f) and Point (%f, %f)" x1 y1 x2 y2
+  if args.Length < 4 then
+    eprintfn "%s" "Number of input paramenters is too short"
+  else
+    let x1, y1, x2, y2 = (args.[0] |> float), (args.[1] |> float), (args.[2] |> float), (args.[3] |> float)
 
-  let result = distance x1 y1 x2 y2
+    printfn "Finding the distance between Point (%f, %f) and Point (%f, %f)" x1 y1 x2 y2
 
-  printfn "The distance between Point (%f, %f) and Point (%f, %f) is %f" x1 y1 x2 y2 result
+    let result = distance x1 y1 x2 y2
+
+    printfn "The distance between Point (%f, %f) and Point (%f, %f) is %f" x1 y1 x2 y2 result
 
 main()
