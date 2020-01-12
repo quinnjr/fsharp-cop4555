3456238c6f4f1bbaeac135fdcfa85431021cc7d5
Joseph R. Quinn <423821+quinnjr@users.noreply.github.com>
2020-01-12 12:49:56 -0500

    Changing distance_calc to use a custom Point type.

--- examples/distance_calc.fsx
+++ examples/distance_calc.fsx
@@ -4,14 +4,16 @@
 // `x1` `y1` `x2` `y2`, where x's are the x-coordinates of the two
 // two points and y's are the y coordinates of the two points.
 
+// A simple 2D point
+type Point = { x: float; y: float }
+
 // Calculate the distance between Point 1 and Point 2.
 // Note: Powers in F# on floats use the pown function.
 // - ** is for floating point values, not integers.
 // - ^ is not usable.
-let distance (x1: float) (y1: float) (x2: float) (y2: float) : float =
-  let x_f = pown (x2 - x1) 2
-  let y_f = pown (y2 - y1) 2
-
+let distance (p1: Point) (p2: Point) : float =
+  let x_f = pown (p2.x - p1.x) 2
+  let y_f = pown (p2.y - p1.y) 2
   sqrt (x_f + y_f)
 
 let main () =
@@ -22,12 +24,19 @@ let main () =
   if args.Length < 4 then
     eprintfn "%s" "Number of input paramenters is too short"
   else
-    let x1, y1, x2, y2 = (args.[0] |> float), (args.[1] |> float), (args.[2] |> float), (args.[3] |> float)
+    // Define Point #1
+    let point1: Point =
+      { x = (args.[0] |> float);
+        y = (args.[1] |> float) }
+    // Define Point #2
+    let point2: Point =
+      { x = (args.[2] |> float);
+        y = (args.[3] |> float) }
 
-    printfn "Finding the distance between Point (%f, %f) and Point (%f, %f)" x1 y1 x2 y2
+    printfn "Finding the distance between Point (%f, %f) and Point (%f, %f)" point1.x point1.y point2.x point2.y
 
-    let result = distance x1 y1 x2 y2
+    let result = distance point1 point2
 
-    printfn "The distance between Point (%f, %f) and Point (%f, %f) is %f" x1 y1 x2 y2 result
+    printfn "The distance between Point (%f, %f) and Point (%f, %f) is %f" point1.x point1.y point2.x point2.y result
 
 main()
