d739c9875ee16c0e68f16571af870751fc43e7e7
Joseph R. Quinn <423821+quinnjr@users.noreply.github.com>
2020-01-12 13:17:34 -0500

    Changing distance_calc to use a Point list. Switching from an if-else statement for control flow to a match statement.

--- examples/distance_calc.fsx
+++ examples/distance_calc.fsx
@@ -11,9 +11,9 @@ type Point = { x: float; y: float }
 // Note: Powers in F# on floats use the pown function.
 // - ** is for floating point values, not integers.
 // - ^ is not usable.
-let distance (p1: Point) (p2: Point) : float =
-  let x_f = pown (p2.x - p1.x) 2
-  let y_f = pown (p2.y - p1.y) 2
+let distance (p: Point list) : float =
+  let x_f = pown (p.[1].x - p.[0].x) 2
+  let y_f = pown (p.[1].y - p.[0].y) 2
   sqrt (x_f + y_f)
 
 let main () =
@@ -21,22 +21,19 @@ let main () =
   // the first element (the command executing).
   let args: string array = fsi.CommandLineArgs |> Array.tail
 
-  if args.Length < 4 then
-    eprintfn "%s" "Number of input paramenters is too short"
-  else
-    // Define Point #1
-    let point1: Point =
-      { x = (args.[0] |> float);
-        y = (args.[1] |> float) }
-    // Define Point #2
-    let point2: Point =
-      { x = (args.[2] |> float);
-        y = (args.[3] |> float) }
+  match args.Length < 4 with
+  | true -> eprintfn "%s" "Number of input paramenters is too short"
+  | false ->
+    // Define our points
+    let points = [
+      { x = (args.[0] |> float); y = (args.[1] |> float) };
+      { x = (args.[2] |> float); y = (args.[3] |> float) }
+    ]
 
-    printfn "Finding the distance between Point (%f, %f) and Point (%f, %f)" point1.x point1.y point2.x point2.y
+    printfn "Finding the distance between Point (%f, %f) and Point (%f, %f)" points.[0].x points.[0].y points.[1].x points.[1].y
 
-    let result = distance point1 point2
+    let result = distance points
 
-    printfn "The distance between Point (%f, %f) and Point (%f, %f) is %f" point1.x point1.y point2.x point2.y result
+    printfn "The distance between Point (%f, %f) and Point (%f, %f) is %f" points.[0].x points.[0].y points.[1].x points.[1].y result
 
 main()
