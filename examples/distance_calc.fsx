0b0a75ccff76cba51dbcbb4e0a466181508e7077
Joseph R. Quinn <423821+quinnjr@users.noreply.github.com>
2020-01-13 10:44:49 -0500

    Moving Point to a common namespace 'QuinnJR.Common'. Changing Point type to Point2D type.  Refactoring to match move of common namespace. Adding Option<'a> type to common namespace.

--- examples/distance_calc.fsx
+++ examples/distance_calc.fsx
@@ -3,15 +3,15 @@
 // Input from the commandline comes in the form of
 // `x1` `y1` `x2` `y2`, where x's are the x-coordinates of the two
 // two points and y's are the y coordinates of the two points.
+#load "../common.fs"
 
-// A simple 2D point
-type Point = { x: float; y: float }
+open QuinnJR.Common
 
 // Calculate the distance between Point 1 and Point 2.
 // Note: Powers in F# on floats use the pown function.
 // - ** is for floating point values, not integers.
 // - ^ is not usable.
-let distance (p: Point list) : float =
+let distance (p: Point2D list) : float =
   let x_f = pown (p.[1].x - p.[0].x) 2
   let y_f = pown (p.[1].y - p.[0].y) 2
   sqrt (x_f + y_f)
