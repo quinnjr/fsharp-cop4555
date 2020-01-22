8bfa7b40351c85f317d9a1a4e58ffe585aebcbce
Joseph R. Quinn <423821+quinnjr@users.noreply.github.com>
2020-01-22 18:12:21 -0500

    Finalizing 06_largest_list

--- introductory_problems/06_largest_list.fs
+++ introductory_problems/06_largest_list.fs
@@ -8,13 +8,14 @@
 // - Check to see if the next item in the list is greater in value than the
 //   previous value. If so, call find_largest with the new largest value and the
 //   next index.
-let rec find_largest prev next list =
+let rec find_largest (prev: int) (next: int) (list: int list) =
   match next <> list.Length with
   | true ->
+    let next_item = list.Item(next)
     match prev > list.Item(next) with
     | true -> find_largest prev (next+1) list
     | false ->
-      find_largest list.Item(next) (next+1) list
+      find_largest next_item (next+1) list
   | false -> prev
 
 [<EntryPoint>]
@@ -22,4 +23,6 @@ let main args =
   let test_list = [5;55;66;101;4;323;67;99;102;53;90;83;67;35;76]
   let result = find_largest 0 1 test_list
 
-  printfn "The largest value is %n" result
+  printfn "The largest value is %d" result
+
+  0
