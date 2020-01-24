6dc32d7569d434a76c71b6631fad71671351be05
Joseph R. Quinn <423821+quinnjr@users.noreply.github.com>
2020-01-24 15:14:51 -0500

    Completing FAKE build integration. Fixing examples which were complaining during compilation.

---
+++ build.fsx
@@ -0,0 +1,39 @@
+// Build.fsx using FAKE Domain-specific language for generating
+// executables of class projects.
+
+#r "paket:
+nuget Fake.Core.Target
+nuget Fake.DotNet.MSBuild
+nuget Fake.IO.Filesystem //"
+#load "./.fake/build.fsx/intellisense.fsx"
+
+open Fake.Core
+open Fake.Core.TargetOperators
+open Fake.DotNet
+open Fake.IO
+open Fake.IO.Globbing.Operators
+
+// *** Define Targets ***
+Target.create "Clean" (fun _ ->
+  Trace.log " --- Cleaning stuff --- "
+  !! "**/*.fs"
+  |> Seq.map (fun inp -> Fake.Core.String.replace ".fs" "" inp)
+  |> Seq.iter (fun exe -> Shell.rm exe)
+)
+
+Target.create "Build" (fun _ ->
+  Trace.log " --- Building the app --- "
+  !! "**/*.fs"
+  |> Seq.iter (fun input ->
+    let output = Fake.Core.String.replace ".fs" "" input
+    Shell.Exec("fsharpc", "--nologo --target:exe -o:" + output + " " + input) |> ignore
+    printfn "AppBuild-Output: %s -> %s" input output
+  )
+)
+
+// *** Define Dependencies ***
+"Clean"
+  ==> "Build"
+
+// *** Start Build ***
+Target.runOrDefault "Build"
