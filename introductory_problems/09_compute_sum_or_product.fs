bf2539e30ace90e40afd1414c19bf669c5940e3a
Joseph R. Quinn <423821+quinnjr@users.noreply.github.com>
2020-01-26 15:55:22 -0500

    Starting 09_cmpute_sum_or_product based off of previous work

---
+++ introductory_problems/09_compute_sum_or_product.fs
@@ -0,0 +1,28 @@
+// Compute the sum or the product of 1 to n values
+// with n being a number supplied by user input
+
+open System
+
+let product = function
+| 1 -> 1
+| i -> i * product (i - 1)
+
+let sum = function
+| 1 -> 1
+| i -> i + sum (i - 1)
+
+[<EntryPoint>]
+let main args =
+  printfn "Input a number: "
+  match System.Console.ReadLine() |> System.Int32.TryParse with
+  | (true, i) ->
+    printfn "What operation would you like to perform? (sum/product)"
+    let operation = System.Console.ReadLine()
+    let result =
+      match operation with
+      | o = "product" -> product i
+      | o = "sum" -> sum i
+      | _ = -> eprintfn "Operation specified is not valid." -1
+    printfn "The result of running %s on %d is: %d" operation i result
+    0
+  | (false, _) -> eprintfn "Non-parseable value supplied"; -1
