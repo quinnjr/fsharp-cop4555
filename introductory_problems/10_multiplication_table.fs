871480cacba4dbe5bff6418e2d583fda0de8667a
Joseph R. Quinn <423821+quinnjr@users.noreply.github.com>
2020-01-26 19:57:40 -0500

    Finalizing 10_multiplication_table

--- introductory_problems/10_multiplication_table.fs
+++ introductory_problems/10_multiplication_table.fs
@@ -1,13 +1,15 @@
 // Write a program that prints a multiplication table for numbers up to 12.
 
 let rec print_table numbers step =
-  match step
-  | 1 -> printfn "%A" numbers
-  | n -> printfn "%A" [ for i in numbers -> i * step ]; print_table numbers (step - 1)
-
+  if step > 0 then
+    printfn "%A" [ for i in numbers -> i * step ]
+    print_table numbers (step - 1)
+  else printf ""
 
 [<EntryPoint>]
-let main =
+let main args =
   let numbers = [ 1..12 ]
 
   print_table numbers 12
+
+  0
