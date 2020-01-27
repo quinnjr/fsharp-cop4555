4d0a8963fc08a6feacdb432eae885a1e32a4d921
Joseph R. Quinn <423821+quinnjr@users.noreply.github.com>
2020-01-27 15:43:54 -0500

    Finalizing implementation of 13_print_leap_years

--- introductory_problems/13_print_leap_years.fs
+++ introductory_problems/13_print_leap_years.fs
@@ -2,21 +2,32 @@
 
 // A leap year is exactly divisible by for except for centry years.
 // For centry years, a leap year must be divisible by 400.
-let is_leap_year y = function
-| (y % 4) = 0 | ((y % 100) = 0 & (y % 400)) = 0 -> true
-| _ -> false
+let is_leap_year year =
+  match year with
+  | y when (y % 4) = 0 || ((y % 100) = 0 && (y % 400) = 0) -> true
+  | _ -> false
 
 [<EntryPoint>]
 let main args =
   let start_year = 2020
   let end_year = start_year + 30
-  let leap_years = [
-    for y in start_year .. end_year -> if is_leap_year y then y else null
-  ]
+
+  let rec leap_years year li =
+    if year = (end_year + 1) then li
+    else
+      let guard = is_leap_year year
+      match year with
+      | y when guard ->
+        let li = li @ [y]
+        leap_years (year + 1) li
+      | y -> leap_years (year + 1) li
+      | _ -> li
 
   printfn "Leap years from %d to %d" start_year end_year
 
-  for y in leap_years do
+  let lst = leap_years 2020 []
+
+  for y in lst do
     printf "%d " y
 
   0
