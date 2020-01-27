9b8aabc266c68bd0254d8f8e2bbcf9c2860b3947
Joseph R. Quinn <423821+quinnjr@users.noreply.github.com>
2020-01-27 14:01:56 -0500

    Finishing work on 12_guessing_game

--- introductory_problems/12_guessing_game.fs
+++ introductory_problems/12_guessing_game.fs
@@ -6,29 +6,41 @@
 
 open System
 
+// Parse input from commandline while gracefully handling invalid
+// input.
+let rec parse_input () =
+  printf "Guess a secret number: "
+  match Console.ReadLine() |> System.Int32.TryParse with
+  | (true, n) -> n
+  | _ -> printfn "Error: could not parse input"; parse_input ()
+
+let rec guess (s: int) (gl: int list) =
+
+  // Add a new value to the guess list if the value is not already
+  // present in the list
+  let rec new_list i l =
+    match l with
+    | head::tail -> if i = head then gl else new_list i tail
+    | [] -> gl @ [i]
+
+  match parse_input () with
+  | i when i = s ->
+    printfn "You guessed the secret number!"
+    printfn "It took you %d guesses to guess the secret." (gl.Length + 1)
+    0
+  | i when i < s ->
+    printfn "Supplied guess is smaller than the secret. Try again."
+    let nl = new_list i gl
+    guess s nl
+  | i when i > s ->
+    printfn "Supplied guess is larger than the secret. Try again."
+    let nl = new_list i gl
+    guess s nl
+  | _ -> failwith "Here be dragons."
+
 [<EntryPoint>]
 let main args =
   let rnd = System.Random()
-  let secret = rnd.Next(20)
-  let guesses = []
+  let secret = rnd.Next(1, 20)
 
-  let rec check_guess i list = function
-  | [] -> i @ list
-  | head::tail -> if head <> i then check_guess i tail else list
-
-  let loop_me =
-    printf "Guess a secret number: "
-    let guess = Console.ReadLine() |> int
-    match guess with
-    | _ -> printfn "You guessed the secret number!"; guesses
-    | secret <> guess ->
-      if secret < guess then
-        printfn "Supplied guess is smaller than the secret. Try again."
-      else
-        printfn "Supplied guess is larger than the secret. Try again."
-      check_guess guess
-      loop_me
-
-  loop_me
-
-  0
+  guess secret []
