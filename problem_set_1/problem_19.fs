eef22c8579773cf9a36820b4a048adaafd697e66
Joseph R. Quinn <423821+quinnjr@users.noreply.github.com>
2020-01-27 19:46:07 -0500

    Commiting work before debugging Problem 19

--- problem_set_1/problem_19.fs
+++ problem_set_1/problem_19.fs
@@ -1,13 +1,9 @@
 // Write an F# function cut xs that cuts a list into two equal parts.
 
 let rec cut in =
-  let gencut i xs =
-    let a = xs.[.. i]
-    let b = xs.[(i + 1) ..]
-    a, b
-
-
-
+  let gencut i xs = function
+  | head::tail when i < in.Length -> gencut (i + 1) (head::xs) tail
+  | x -> (List.rev xs), x
 
 [<EntryPoint>]
 let main args =
