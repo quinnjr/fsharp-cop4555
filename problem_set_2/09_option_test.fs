d45b0a2cbd8c4790655d1fb0948015982a1f3f82
Joseph R. Quinn <423821+quinnjr@users.noreply.github.com>
2020-02-12 17:38:42 -0500

    Commiting final working answer to problem set #9

--- problem_set_2/09_option_test.fs
+++ problem_set_2/09_option_test.fs
@@ -4,13 +4,10 @@ let list_print = function
 | (xs, None) -> printfn "The last element of %A is %s." xs "\"Invalid Input\""
 | (xs, Some(value)) -> printfn "The last element of %A is %A." xs value
 
-let check_list (xs: 'a list): 'a option =
-  if xs.IsEmpty then None else Some(xs.Item(xs.Length - 1))
-
-let rec check_list2 = function
+let rec check_list = function
 | [] -> None
 | [x] -> Some(x)
-| x::xs -> check_list2 xs
+| x::xs -> check_list xs
 
 [<EntryPoint>]
 let main args =
@@ -22,7 +19,5 @@ let main args =
   list_print (li1, check_list li1)
   list_print (li2, check_list li2)
   list_print (li3, check_list li3)
-
-  list_print li1, check_list2 li1
 
   0
