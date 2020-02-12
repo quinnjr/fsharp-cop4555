9564e3abae8634f5a723c928e6cbdf88158b1e3c
Joseph R. Quinn <423821+quinnjr@users.noreply.github.com>
2020-02-12 17:34:19 -0500

    Commiting final working answer to problem set #9

--- problem_set_2/09_option_test.fs
+++ problem_set_2/09_option_test.fs
@@ -1,28 +1,21 @@
 // Project to show the uses of the option type.
 
-let list_print xs x =
-  match x with
-  | None -> printfn "The last element of %A is %s." xs "\"Invalid Input\""
-  | Some(value) -> printfn "The last element of %A is %A." xs value
+let list_print = function
+| (xs, None) -> printfn "The last element of %A is %s." xs "\"Invalid Input\""
+| (xs, Some(value)) -> printfn "The last element of %A is %A." xs value
 
-let rec check_list (xs: 'a list): 'a =
-  match xs with
-  | []::_ -> None
-  | x::[] -> Some(x)
-  | _::xs -> check_list xs
+let check_list (xs: 'a list): 'a option =
+  if xs.IsEmpty then None else Some(xs.Item(xs.Length - 1))
 
+[<EntryPoint>]
 let main args =
 
   let li1 = []
   let li2 = ["cat";]
   let li3 = [1;2;3;4;5]
 
-  let res1 = check_list li1
-  (*
-  let res2 = check_list li2
-  let res3 = check_list li3
-  *)
-
-  list_print li1 res1
+  list_print (li1, check_list li1)
+  list_print (li2, check_list li2)
+  list_print (li3, check_list li3)
 
   0
