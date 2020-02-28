bc39e7180067fca517732baeb6a63cd6d7b1bbfe
Joseph R. Quinn <423821+quinnjr@users.noreply.github.com>
2020-02-28 10:09:13 -0500

    Continuing work on problem 09 with a discriminated union

--- problem_set_2/09_option_test.fs
+++ problem_set_2/09_option_test.fs
@@ -1,8 +1,13 @@
 // Project to show the uses of the option type.
 
+type IntOrString = IsInt of int | IsString of string
+
 let rec check_list = function
 | [] -> None
-| [x] -> Some(x)
+| [x] ->
+  match x with
+  | IsInt(i) -> Some(sprintf "%i" i)
+  | IsString(s) -> Some s
 | _::xs -> check_list xs
 
 [<EntryPoint>]
@@ -11,7 +16,7 @@ let main args =
   let print_check_list xs =
     match check_list xs with
     | None -> printfn "The last element of %A is %s." xs "\"Invalid Input\""
-    | Some(x) -> printfn "The last element of %A is %A." xs x
+    | Some(x) -> printfn "The last element of %A is %s." xs x
 
   print_check_list []
   print_check_list ["cat"]
