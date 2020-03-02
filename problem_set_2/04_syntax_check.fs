4d8da48b9d24785e82a83ac80747341d27fb1402
Joseph R. Quinn <423821+quinnjr@users.noreply.github.com>
2020-03-02 15:01:21 -0500

    Continuing work on 05_vector_product

---
+++ problem_set_2/04_syntax_check.fs
@@ -0,0 +1,5 @@
+(*
+Implement a syntax checker using functional programming and immutable data for
+the unambiguous grammar for arithmetic expressions, from the Notes on
+Programming Language Syntax.
+*)
---
+++ problem_set_2/05_vector_product.fs
@@ -0,0 +1,26 @@
+(*
+Given vectors u = (u1, u2,..., un) and v = (v1, v2,..., vn),
+the inner product of u and v is defined to be u1*v1 + u2*v2 + ... + un*vn.
+Write a curried F# function inner that takes two vectors represented as
+int list and returns their inner product.
+*)
+
+(* let rec sigma acc = function
+| ([], []) | (_, []) | ([], _) -> 0
+| ([x], [y]) -> x * y
+| (x::xs, y::ys) -> (x * y) + sigma (xs, ys) *)
+
+let rec sigma acc = function
+| ([], []) | (_, []) | ([], _) -> acc
+| (x::xs, y::ys) -> sigma ((x * y) + acc) (xs, ys)
+
+let rec inner xs ys =
+  if List.length xs <> List.length ys then
+    failwith "Exception: Lists are of unequal length"
+  else sigma 0 (xs, ys)
+
+[<EntryPoint>]
+let main _ =
+  printfn "%A" inner [1I..50000I] [50001I..100000I]
+
+  0
