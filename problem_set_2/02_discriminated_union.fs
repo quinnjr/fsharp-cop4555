70c2ca30a1962a01dd15cf9fff05c1cd18c38b84
Joseph R. Quinn <423821+quinnjr@users.noreply.github.com>
2020-03-02 14:34:17 -0500

    Continuing genericizing 02_discriminated_union

--- problem_set_2/02_discriminated_union.fs
+++ problem_set_2/02_discriminated_union.fs
@@ -11,20 +11,20 @@ Apply the function to the Coordinate like List.reduce.
 
 Call the function with (+) for each of the Coordinates in part (b).
 
-Call the function with (-) for the numeric Coordinates in part (b). Be sure
-that your function implements the normal associativity for (-).
+Call the function with (-) for the numeric Coordinates in part (b).
+
+Be sure that your function implements the normal associativity for (-).
 *)
 
-type Coordinate<'a> =
-| Tuple of 'a * 'a
-| Triple of 'a * 'a * 'a
-| Quadruple of 'a * 'a * 'a * 'a
-
-let reduce_coordinate f (t: Coordinate<'a>): 'a =
-  match t with
-  | Tuple (x, y) -> f(x, f(y, Unchecked.defaultof<'a>))
-  | Triple (x, y, z) -> f(x, f(y, f(z, Unchecked.defaultof<'a>)))
-  | Quadruple (w, x, y, z) -> f(w, f(x, f(y, f(z, Unchecked.defaultof<'a>))))
+type Coordinate<'T> =
+  | Tuple of 'T * 'T
+  | Triple of 'T * 'T * 'T
+  | Quadruple of 'T * 'T * 'T * 'T
+  static member reduce_coordinate (f: 'T -> 'T -> 'T) (coord: Coordinate<'T>): 'T =
+    match coord with
+    | Tuple (x, y) -> f(x, f(y, Unchecked.defaultof<'T>))
+    | Triple (x, y, z) -> f(x, f(y, f(z, Unchecked.defaultof<'T>)))
+    | Quadruple (w, x, y, z) -> f(w, f(x, f(y, f(z, Unchecked.defaultof<'T>))))
 
 [<EntryPoint>]
 let main _ =
@@ -33,11 +33,11 @@ let main _ =
   let tripl = Coordinate.Triple (1.0, 2.0, 3.0)
   let quadrpl = Coordinate.Quadruple ("0", "1", "2", "3")
 
-  let tupl_red1 = reduce_coordinate(fun acc elem -> acc + elem) tupl
+  let tupl_red1 = Coordinate.reduce_coordinate (fun acc elem -> acc + elem) tupl
   let tupl_red2 = reduce_coordinate (fun acc elem -> acc - elem) tupl
 
-  let tripl_red1 = reduce_coordinate(fun acc elem -> acc + elem) tripl
-  let tripl_red2 = reduce_coordinate(fun acc elem -> acc - elem) tripl
+  let tripl_red1 = Coordinate.reduce_coordinate (fun acc elem -> acc + elem) tripl
+  let tripl_red2 = Coordinate.reduce_coordinate (fun acc elem -> acc - elem) tripl
 
   printfn "%A" tupl
   printfn "%A" tripl
