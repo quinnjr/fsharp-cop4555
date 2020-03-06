efc57d642946caf8cc08ae2786f60fd26f5dbf9e
Joseph R. Quinn <423821+quinnjr@users.noreply.github.com>
2020-03-06 10:58:07 -0500

    Moving over Probelem03
    Moving over Problem05
    
    Moving over Problem06
    
    Moving over Problem9
    
    Moving over Problem11

--- src/ProblemSet2.fs
+++ src/ProblemSet2.fs
@@ -155,10 +155,34 @@ module Problem06 =
 (*
 module Problem07
   let test () =
-
+*)
 module Problem09
-  let test () =
+  let rec check_list = function
+  | [] -> None
+  | [x] -> Some(sprintf "%A" x)
+  | _::xs -> check_list xs
 
+  let printNone xs = printfn "The last element of %A is %s." xs "\"Invalid Input\""
+  let printSome xs x = printfn "The last element of %A is %s." xs x
+
+  let test () =
+    let list1 = []
+    let list2 = ["cat"]
+    let list3 = [1..5]
+
+    match check_list list1 with
+    | None -> printNone list1
+    | Some(x) -> printSome list1 x
+
+    match check_list list2 with
+    | None -> printNone list2
+    | Some(x) -> printSome list2 x
+
+    match check_list list3 with
+    | None -> printNone list3
+    | Some(x) -> printSome list3 x
+
+(*
 module Problem10
   let test () =
 *)
